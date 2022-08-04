6412e07eef7b46ba34aad1abcf8018f4a9f9134d
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-08-04 18:43:01 -0400

    Updates

--- precache-manifest.1efaa4f0f2f9636ecac17551a2b522c3.js
+++ precache-manifest.1efaa4f0f2f9636ecac17551a2b522c3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82f7763abb09ed81f4ac5229272e2486",
+    "revision": "65be3040c72661c71a4568b2ac0632c4",
     "url": "./index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.3a7190d4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "47804a8116708784eb16",
-    "url": "./static/js/main.373cffb6.chunk.js"
+    "revision": "bbb0d00b4b2e5efb4642",
+    "url": "./static/js/main.c3f51250.chunk.js"
   },
   {
     "revision": "d6b97dd2e6ca00c8b0b2",
