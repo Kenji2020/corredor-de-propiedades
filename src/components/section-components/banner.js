ed20be8813cebfeacdc0305ebdc19cdd01b2610e
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-08-04 21:24:41 -0400

    a

--- src/components/section-components/banner.js
+++ src/components/section-components/banner.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import sectiondata from '../../data/sections.json';
 import parse from 'html-react-parser';
-
-class Banner extends Component {
-
+import axios from 'axios';
+class Banner   extends Component {
+    state = {
+      datos:{a:"a"}
+    }
     componentDidMount() {
+      axios.get('https://part1backend.herokuapp.com/banners')
+      .then(res => {
+          const data2 = res.data[Object.keys(res.data)[0]]
+          const values = Object.keys(data2)
+  
+         let datos = {}
+          values.forEach((key, index) => {
+            datos[key] = data2[key]
+          })
+          console.log(datos)
+          this.setState({ datos })
 
+      })
     const $ = window.$;
     
      if ($('.single-select').length){
@@ -15,6 +29,7 @@ class Banner extends Component {
   }
 
     render() {
+      console.log(this.state.datos)
 
         let publicUrl = process.env.PUBLIC_URL+'/'
         let imagealt = 'image'
