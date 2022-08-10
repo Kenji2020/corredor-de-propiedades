a1b2c8c511c0bbf019d080377a63f272d0c57197
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-08-10 17:13:56 -0400

    videos

--- src/components/section-components/carrusel.js
+++ src/components/section-components/carrusel.js
@@ -2,81 +2,18 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
-
+import fondo from '../../volcanes.mp4'
+import { Player } from 'video-react';
+import "../../../node_modules/video-react/dist/video-react.css";
+import "../../App.css"
 export const DemoCarousel = () => {
   return (
-    <div
-      id="carouselExampleIndicators"
-      class="carousel slide"
-      data-ride="carousel"
-      interval={2000}
-      ride={false}
-    >
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img
-            class="d-block w-100"
-            src="/assets/img/banner/31.jpg"
-            alt="First slide"
-          />
-          <div class="carousel-caption d-none d-md-block" style={{'marginBottom':'100px'}}>
-            <h5 className="sub-title" style={{ color: "rgba(255,255,255)" }}>
-              Encontrarás el lugar perfecto
-            </h5>
-            <h1 className="title" style={{ color: "white" }}>
-              Para tu nuevo hogar
-            </h1>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img
-            class="d-block w-100"
-            src="/assets/img/Lago galletue campo/3.jpg"
-            alt="Second slide"
-          />
-          <div class="carousel-caption d-none d-md-block" style={{'marginBottom':'100px'}}>
-            <h5 className="sub-title" style={{ color: "rgba(255,255,255)" }}>
-              Conocerás lugares hermosos
-            </h5>
-            <h1 className="title" style={{ color: "white" }}>
-              Donde vivir tranquilamente
-            </h1>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img
-            class="d-block w-100"
-            src="/assets/img/Parcelas lago maihue/2.jpg"
-            alt="Third slide"
-          />
-          <div class="carousel-caption d-none d-md-block" style={{'marginBottom':'100px'}}>
-            <h5 className="sub-title" style={{ color: "rgba(255,255,255)" }}>
-              Solo deja todo en nuestras manos
-            </h5>
-            <h1 className="title" style={{ color: "white" }}>
-              Nosotros nos encargamos de todo
-            </h1>
-          </div>
-        </div>
-      </div>
-      <a
-        class="carousel-control-prev"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Anterior</span>
-      </a>
-      <a
-        class="carousel-control-next"
-        href="#carouselExampleIndicators"
-        role="button"
-        data-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Siguiente</span>
-      </a>
+   <div>
+    <div className="overlay"></div>
+    <video className="videito" src={fondo} autoPlay loop muted/>
+    <div className="content">
+      Vivir en el sur 
     </div>
+  </div>
   );
 };
