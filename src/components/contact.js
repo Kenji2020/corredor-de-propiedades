e589ea6a6aac07a2588322f3e2e4f724a4f2f7db
dlwlrmla <joakomask@gmail.com>
2022-08-18 20:53:01 -0400

    validacion form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,4 @@
-import React,{useRef} from "react";
+import React,{useRef, useState} from "react";
 import emailjs from '@emailjs/browser';
 
 import Navbar from "./global-components/navbar";
@@ -7,15 +7,30 @@ import FaqSection from "./section-components/faq";
 import Footer from "./global-components/footer";
 
 const Contact = () => {
+
+  const [data, setData ] = useState({
+    user_name : '',
+    user_email : "",
+    message : ''
+  })
+
   const form = useRef();
   const ref = useRef();
   const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_htvx45q', 'template_6a6uu6p', form.current, 'SdA7vrvy_dyS3YLf1')
+    
+    if(data.user_name.length <2 || data.user_email.length <2 || data.message.length <2){
+      alert('campos vacios o incorrectos')
+      return
+    }
+    emailjs.send('service_htvx45q', 'template_6a6uu6p', data, 'SdA7vrvy_dyS3YLf1')
       .then((result) => {
-          console.log(result.text);
+          //console.log(data.user_name);
           alert('Correo enviado correctamente')
+          setData({
+            user_name : '',
+            user_email : "",
+            message : ''
+          })
       }, (error) => {
           console.log(error.text);
           alert('Ha ocurrido un error, inténtalo  más tarde')
@@ -41,16 +56,16 @@ const Contact = () => {
               </div>
             </div>
             <div className="col-lg-4">
-              <form className="contact-form-wrap contact-form-bg" ref={form} onSubmit={sendEmail}>
+              <form className="contact-form-wrap contact-form-bg" ref={form} onSubmit={(e) => sendEmail(e)}>
                 <h4>Contáctanos</h4>
                 <div className="rld-single-input">
-                  <input type="text" placeholder="Nombre" name="user_name" />
+                  <input type="text" placeholder="Nombre"  onChange={(e) => setData({...data, user_name: e.target.value})} name="user_name" />
                 </div>
                 <div className="rld-single-input">
-                  <input type="email" placeholder="Correo electrónico" name="user_email"/>
+                  <input type="email" placeholder="Correo electrónico"  onChange={(e) => setData({...data, user_email: e.target.value})} name="user_email"/>
                 </div>
                 <div className="rld-single-input">
-                  <textarea rows={10} placeholder="Mensaje" name="message" defaultValue={""} />
+                  <textarea rows={10} placeholder="Mensaje" name="message" onChange={(e) => setData({...data, message : e.target.value })}  defaultValue={""} />
                 </div>
                 <div className="btn-wrap text-center">
                   <button className="btn btn-success" value="Send" type="submit">Enviar</button>
